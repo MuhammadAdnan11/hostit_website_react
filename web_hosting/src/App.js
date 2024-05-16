404ca0c27a31194ded16ee010f504aba81f1d3b0
MuhammadAdnan11 <adnan191022@gmail.com>
2024-05-16 14:51:31 +0500

    updated

--- web_hosting/src/App.js
+++ web_hosting/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Banner from "./Components/Banner/Banner";
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  );
 }
 
 export default App;
