4d5ff568a5ec0974dbcba35141e2423e7dd9b9c0
MuhammadAdnan11 <adnan191022@gmail.com>
2024-05-18 13:58:31 +0500

    updated

--- web_hosting/src/Components/Banner/Banner.js
+++ web_hosting/src/Components/Banner/Banner.js
@@ -4,27 +4,31 @@ const Banner = () => {
   return (
     <>
       <div className="container-fluid banner-container">
-        <div className="row ">
-          <div className="col-md-6 text-white">
-            <h1>
-              Fast & Secure <br />
-              Web Hosting
-            </h1>
-            <p>
-              Anything embarrassing hidden in the middle of text. All the Lorem
-              Ipsuanything embarrassing hidden in the middle of text. All the
-              Lorem Ipsumm
-            </p>
-            <div className="row">
-              <div className="col-md-5 btn_banner_ReadMore">
-                <button>Read More</button>
-              </div>
-              <div className="col-md-5 btn_banner_ContactUs">
-                <button>Contact Us</button>
+        <div className="container">
+          <div className="row ">
+            <div className="col-md-6 text-white bg-success my-5 pt-5">
+              <h2 className="mt-5 pt-3">
+                Fast & Secure <br />
+                Web Hosting
+              </h2>
+              <p className="mt-4">
+                Anything embarrassing hidden in the middle of text. All the
+                Lorem Ipsuanything embarrassing hidden in the middle of text.
+                All the Lorem Ipsumm
+              </p>
+              <div className="row my-4 ">
+                <div className="col-md-4 btn_banner_ReadMore">
+                  <button>Read More</button>
+                </div>
+                <div className="col-md-4 btn_banner_ContactUs">
+                  <button>Contact Us</button>
+                </div>
               </div>
             </div>
+            <div className="col-md-6 bg-secondary">
+              <img src="./images/slider-img.png"></img>
+            </div>
           </div>
-          <div className="col-md-6"></div>
         </div>
       </div>
     </>
